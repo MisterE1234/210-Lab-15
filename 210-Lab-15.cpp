725838989f68bcc4b32c4ffa0c0d70bb4a26e81a
MisterE1234 <MisterE1234@outlook.com>
2025-09-27 20:20:41 -0700

    Initial Commit

---
+++ 210-Lab-15.cpp
@@ -0,0 +1,3 @@
+//COMSC-210|Lab-15|Ethan Aylard
+//IDE used: Visual Studio Code
+//Program description: This program demonstrates the use of classes and input files together. I will show a movie class with data provided from Prof. Dietrich.
